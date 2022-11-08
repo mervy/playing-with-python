42a29bb1b94caba239eaea101af787466f0bde19
Rogério Soares <rgrsoares@yahoo.com.br>
2022-11-08 18:30:37 -0300

    Create strongPassword.py

---
+++ bruteforce/strongPassword.py
@@ -0,0 +1,19 @@
+# https://academiahopper.com.br/random-como-gerar-e-quebrar-senhas-fortes/
+print("[||||||----Generating Strong Passwords----||||||]")
+
+import string
+from random import choice
+
+lower = string.ascii_lowercase # abcd...
+upper = string.ascii_uppercase # ABCD...
+allLetters = string.ascii_letters # abcd...ABCD...
+digits = string.digits # 0123...
+punctuation = string.punctuation #<=.^...
+
+size = input(f "Qual o tamanho da senha? ")
+types = input("Quais caracteres que a senha terá?[lower, upper, allLetters, digits, punctuation] ")
+password = ''
+
+for i in range(size):
+    password += choice(types)
+print (password)
